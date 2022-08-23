21b2984d91a3a04c8ba3f8c777ba22d5a79ca9b0
swathi-26txt <100840631+swathi-26txt@users.noreply.github.com>
2022-08-23 13:58:40 +0530

    Add files via upload

---
+++ SimpleMFA.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleMFA {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+
+	}
+
+}
